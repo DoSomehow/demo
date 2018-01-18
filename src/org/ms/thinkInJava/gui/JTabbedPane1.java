75207f1066cbcf4f804bb969a488453a25282685
DoSomehow <329237665@qq.com>
2018-01-19 00:34:34 +0800

    practice gui  2018/01/19

---
+++ src/org/ms/thinkInJava/gui/JTabbedPane1.java
@@ -0,0 +1,40 @@
+package org.ms.thinkInJava.gui;
+
+import org.ms.thinkInJava.util.SwingConsole;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+
+public class JTabbedPane1 extends JFrame {
+
+    private String[] flavors = {
+            "Chocolate", "Strawberry", "Vanilla Fudge Swirl",
+            "Mint Chip", "Mocha Almond Fudge", "Rum Raisin",
+            "Praline Cream", "Mud Pie"
+    };
+
+    private JTabbedPane tabs = new JTabbedPane();
+    private JTextField txt = new JTextField(20);
+
+    public JTabbedPane1(){
+        int i = 0;
+        for(String flavor : flavors){
+            tabs.addTab(flavors[i], new JButton("Tabbed pane " + i++));
+        }
+        tabs.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                txt.setText("Tab selected: " + tabs.getSelectedIndex());
+            }
+        });
+        add(BorderLayout.SOUTH, txt);
+        add(tabs);
+    }
+
+
+    public static void main(String[] args) {
+        SwingConsole.run(new JTabbedPane1(), 400, 250);
+    }
+}
---
+++ src/org/ms/thinkInJava/gui/RadioButtons.java
@@ -0,0 +1,41 @@
+package org.ms.thinkInJava.gui;
+
+import org.ms.thinkInJava.util.SwingConsole;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RadioButtons extends JFrame {
+
+    private JTextField t = new JTextField(15);
+    private ButtonGroup g = new ButtonGroup();
+    private JRadioButton rb1 = new JRadioButton("one", false);
+    private JRadioButton rb2 = new JRadioButton("two", false);
+    private JRadioButton rb3 = new JRadioButton("three", false);
+    private ActionListener al = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            t.setText("Radio button " + ((JRadioButton)e.getSource()).getText());
+        }
+    };
+
+    public RadioButtons(){
+        rb1.addActionListener(al);
+        rb2.addActionListener(al);
+        rb3.addActionListener(al);
+        g.add(rb1);g.add(rb2);g.add(rb3);
+        t.setEditable(false);
+        setLayout(new FlowLayout());
+        add(t);
+        add(rb1);
+        add(rb2);
+        add(rb3);
+    }
+
+    public static void main(String[] args) {
+        SwingConsole.run(new RadioButtons(), 200, 125);
+    }
+
+}
