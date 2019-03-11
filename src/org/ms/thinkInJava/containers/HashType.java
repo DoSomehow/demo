f96b1a1f21eb00c2d0ff104f99a09cb32807bab1
DoSomehow <329237665@qq.com>
2019-03-12 00:57:42 +0800

    容器深入研究

---
+++ src/org/ms/thinkInJava/containers/HashType.java
@@ -0,0 +1,13 @@
+package org.ms.thinkInJava.containers;
+
+public class HashType extends SetType {
+
+    public HashType(int n){
+        super(n);
+    }
+
+    public int hashCode(){
+        return i;
+    }
+
+}
